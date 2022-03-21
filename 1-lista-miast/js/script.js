d7cf98e7212d6c856e4d7e4801079fae961913d1
Cama-ro <kam.rybicka@gmail.com>
2022-03-21 10:57:03 +0100

    zadanie 1-lista-miast

---
+++ 1-lista-miast/js/script.js
@@ -0,0 +1,27 @@
+let cities = [
+  "Warsaw",
+  "Berlin",
+  "Paris",
+  "London",
+  "New York",
+  "Tokio",
+  "Moscow",
+];
+
+// console.log(cities);
+
+const content = document.querySelector("div"); // add/collect div
+const list = document.createElement("ol"); // create ol list
+console.log(list);
+
+// loop for create li elements in ol
+for (i = 0; i <= cities.length - 1; i++) {
+  const li = document.createElement("li"); // create li element.
+
+  li.innerHTML = cities[i]; // assigning text to li using array value.
+  li.setAttribute("class", "city"); // add class to li elements.
+
+  list.appendChild(li);
+}
+
+content.appendChild(list);
